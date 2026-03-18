a2256a2c884020a220274cfb16de49ad74ca85dd
Amer Shani Bafi <535640@student.fontys.nl>
2026-03-18 12:58:57 +0100

    Eerste commit ASP.NET API

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace O2W
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
